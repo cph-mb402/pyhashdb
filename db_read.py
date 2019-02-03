9ed66fb63a782cbe804519d109a06fe427ebf860
Vlad Mircea Burac <cph-mb402@cphbusiness.dk>
2019-02-03 13:02:08 +0100

    Update db_read.py
    Add cases for empty database

--- db_read.py
+++ db_read.py
@@ -4,6 +4,8 @@ _hashmap = index_hashmap.get_hashmap()
 # print(_hashmap)
 
 def getPair(index):
+    if hashmap == {}:
+        return "The database is empty"
     file = open("database", "rb")
     # print(file.read())
     file.seek(_hashmap[index], 0)
@@ -13,6 +15,8 @@ def getPair(index):
     return pair
 
 def getValue(index):
+    if hashmap == {}:
+        return "The database is empty"
     return getPair(index).split(",")[1]
 
 print(getValue("key2"))
